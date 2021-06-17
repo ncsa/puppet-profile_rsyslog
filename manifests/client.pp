a877ec02fe1b17d6c6e1d68d25eaf1ed9e7a1886
Matthew Long <mtlong2@illinois.edu>
2021-06-17 12:29:42 -0500

    replaced manifests with the ones from Radiant, and added default data for new manifests

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,11 +1,17 @@
 # @summary Configures an rsyslog client
 #
-# Configures and rsyslog client for NCSA systems
+# Configures an rsyslog client for NCSA systems
 #
 # @example
-#   include profile_rsyslog::client
+#   include profile::rsyslog::client
 #
 # @see https://forge.puppet.com/puppet/rsyslog
+#
+# @param config_actions
+#   A hash of hashes to provide rsyslog actions configuration options
+#
+# @param config_custom
+#   A hash of hashes to provide rsyslog objects that require custom configuration snips
 #
 # @param config_global
 #   A hash of hashes to provide global configuration options
@@ -13,23 +19,38 @@
 # @param config_modules
 #   A hash of hashes to provide rsyslog module configuration options
 #
-# @param config_actions
-#   A hash of hashes to provide rsyslog actions configuration options
+# @param config_rulesets
+#   A hash of hashes to provide rsyslog ruleset configuration options
 #
-class profile_rsyslog::client (
-  Hash $config_global,
-  Hash $config_modules,
-  Hash $config_actions,
+# @param feature_packages
+#   An array of extra "feature" packages to install for rsyslog.
+#
+# @param override_default_config
+#   Override the content of the default config file or not?
+#
+# @param purge_config_files
+#   Purge other config file from the include dir (/etc/rsyslog.d/) or not.
+class profile::rsyslog::client (
+  Hash    $config_actions,
+  Hash    $config_custom,
+  Hash    $config_global,
+  Hash    $config_modules,
+  Hash    $config_rulesets,
+  Array   $feature_packages,
+  Boolean $override_default_config,
+  Boolean $purge_config_files,
 ) {
   class { 'rsyslog':
-    feature_packages        => ['rsyslog-relp'],
-    purge_config_files      => true,
-    override_default_config => true,
+    feature_packages        => $feature_packages,
+    override_default_config => $override_default_config,
+    purge_config_files      => $purge_config_files,
   }
 
   class { 'rsyslog::config':
+    actions       => $config_actions,
+    custom_config => $config_custom,
     global_config => $config_global,
     modules       => $config_modules,
-    actions       => $config_actions,
+    rulesets      => $config_rulesets,
   }
 }
--- manifests/relay.pp
+++ manifests/relay.pp
@@ -1,21 +1,30 @@
-# @summary Configures and NCSA rsyslog relay
+# @summary Configures an NCSA rsyslog relay
 #
 # This class configures an NCSA rsyslog relay to facilitate
 # sending logs to security from machines that are in isolated networks
 #
 # @example
-#   include profile_rsyslog::relay
+#   include profile::rsyslog::relay
 #
 # @see https://forge.puppet.com/puppet/rsyslog
+#
+# @param allow_ip_ranges,
+#   An array of CIDR ranges from which to accept incoming logs.
+#
+# @param config_actions
+#   A hash of hashes to provide rsyslog actions configuration options
+#
+# @param config_custom
+#   A hash of hashes to provide rsyslog objects that require custom configuration snips
 #
 # @param config_global
 #   A hash of hashes to provide global configuration options
 #
+# @param config_inputs
+#   A hash of hashes to provide rsyslog inputs configuration options
+#
 # @param config_modules
 #   A hash of hashes to provide rsyslog module configuration options
-#
-# @param config_actions
-#   A hash of hashes to provide rsyslog actions configuration options
 #
 # @param config_rulesets
 #   A hash of hashes to provide rsyslog rulesets configuration options
@@ -23,54 +32,61 @@
 # @param config_templates
 #   A hash of hashes to provide rsyslog templates configuration options
 #
-# @param config_custom
-#   A hash of hashes to provide rsyslog objects that require custom configuration snips
+# @param feature_packages
+#   An array of extra "feature" packages to install for rsyslog.
 #
-# @param config_inputs
-#   A hash of hashes to provide rsyslog inputs configuration options
+# @param firewall_port_data
+#   A hash of hashes to provide input on ports to open in the firewall.
 #
-# @param relayed_hosts
-#   An array of subnets or hosts allowed to relay logs via this server
-#   Format can be a single IP or CIDR subnet
+# @param override_default_config
+#   Override the content of the default config file or not?
 #
-# @param relay_port
-#   The port number that will be used to listen for connections for relayed syslog
+# @param purge_config_files
+#   Purge other config file from the include dir (/etc/rsyslog.d/) or not.
 #
-class profile_rsyslog::relay (
-  Hash                       $config_global,
-  Hash                       $config_modules,
-  Hash                       $config_actions,
-  Hash                       $config_templates,
-  Hash                       $config_rulesets,
-  Hash                       $config_custom,
-  Hash                       $config_inputs,
-  String                     $relay_port,
-  Array[Stdlib::IP::Address] $relayed_hosts,
+class profile::rsyslog::relay (
+  Array[String] $allow_ip_ranges,
+  Hash          $config_actions,
+  Hash          $config_custom,
+  Hash          $config_global,
+  Hash          $config_inputs,
+  Hash          $config_modules,
+  Hash          $config_rulesets,
+  Hash          $config_templates,
+  Array         $feature_packages,
+  Boolean       $override_default_config,
+  Boolean       $purge_config_files,
+  Hash          $firewall_port_data,
 ) {
 
   class { 'rsyslog':
-    feature_packages        => ['rsyslog-relp'],
-    purge_config_files      => true,
-    override_default_config => true,
+    feature_packages        => $feature_packages,
+    override_default_config => $override_default_config,
+    purge_config_files      => $purge_config_files,
   }
 
   class { 'rsyslog::config':
-    global_config => $config_global,
-    modules       => $config_modules,
     actions       => $config_actions,
-    inputs        => $config_inputs,
-    templates     => $config_templates,
-    rulesets      => $config_rulesets,
     custom_config => $config_custom,
+    global_config => $config_global,
+    inputs        => $config_inputs,
+    modules       => $config_modules,
+    rulesets      => $config_rulesets,
+    templates     => $config_templates,
   }
 
   # Allow rsyslog forwarding
-  $relayed_hosts.each | String $host | {
-    firewall { "500 profile_rsyslog::relay - rsyslog from ${host}":
-      proto  => 'tcp',
-      dport  => $relay_port,
-      action => 'accept',
-      source => $host,
+  $allow_ip_ranges.each | $ip_range | {
+    $firewall_port_data.each | $name, $data | {
+      $port     = $data[port]
+      $protocol = $data[protocol]
+      firewall { "500 profile::rsyslog::relay - allow rsyslog ${name} from ${ip_range}":
+        proto  => $protocol,
+        dport  => $port,
+        action => 'accept',
+        source => "${ip_range}",
+      }
     }
   }
+
 }
